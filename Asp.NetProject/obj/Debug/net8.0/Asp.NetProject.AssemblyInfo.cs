588c27ff075b088f9097bf7a0a4e6e303c56ae54
DUNGCT\ndung <ndung983@gmail.com>
2024-09-17 20:41:48 +0700

    okw

--- Asp.NetProject/obj/Debug/net8.0/Asp.NetProject.AssemblyInfo.cs
+++ Asp.NetProject/obj/Debug/net8.0/Asp.NetProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.NetProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53114336218ff0d9283c2e1aca0031b861aedf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.NetProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.NetProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
