1b72afcc6b8c9b1faaa9ccf7ec935aff1714260c
NguyenXuantiendung <101032317+Dunglailaptop@users.noreply.github.com>
2024-09-18 07:12:35 +0700

    lo

--- Asp.NetProject/obj/Debug/net8.0/Asp.NetProject.AssemblyInfo.cs
+++ Asp.NetProject/obj/Debug/net8.0/Asp.NetProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.NetProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0846374bc81ab747e949080fde5b8d25bff2fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.NetProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.NetProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ServerApi/obj/Debug/net8.0/ServerApi.AssemblyInfo.cs
+++ ServerApi/obj/Debug/net8.0/ServerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53114336218ff0d9283c2e1aca0031b861aedf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0846374bc81ab747e949080fde5b8d25bff2fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
