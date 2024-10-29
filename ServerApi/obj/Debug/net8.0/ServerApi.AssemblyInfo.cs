15aa567393bbff5f027509c533a6ea0cf043bfb8
NguyenXuantiendung <101032317+Dunglailaptop@users.noreply.github.com>
2024-10-29 16:01:12 +0700

    chinhsuanhieucai

--- ServerApi/obj/Debug/net8.0/ServerApi.AssemblyInfo.cs
+++ ServerApi/obj/Debug/net8.0/ServerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b39b0196669f4b470419d3f8cde2401faa44c8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa370efb59f08dccf19452643bab50eedd1fff53")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
