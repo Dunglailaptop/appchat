0a12eaf163ce458acaf3cd5244ff48f3f2440f8c
NguyenXuantiendung <101032317+Dunglailaptop@users.noreply.github.com>
2024-11-01 07:13:07 +0700

    oke

--- ServerApi/obj/Debug/net8.0/ServerApi.AssemblyInfo.cs
+++ ServerApi/obj/Debug/net8.0/ServerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c7a37b529579b3ee365683e833a72e37eed6bb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28706a246b0e7f1f490a93e20fcac63c7576b752")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
